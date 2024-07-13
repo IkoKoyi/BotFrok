7ea4f972c28183a0367d2c4bddb5adb8b64efb53
IkoKoyi <magnumx019@gmail.com>
2024-07-13 23:34:54 +0700

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 import os
 
-TOKEN = os.environ.get("TOKEN", None) # Dapatkan di @botfather
+TOKEN = os.environ.get("TOKEN", "7221053026:AAG1paijAkaCeV2GusX8Ir-MV3tTcC0sDcw") # Dapatkan di @botfather
 OWNER = os.environ.get("OWNER", "skoyi19") # Isi dengan username kalian tanpa tanda @
 GROUP = os.environ.get("GROUP", "Cari_pacar_jodoh_teman") # Isi dengan username group kalian tanpa tanda @ kalau gak punya gak usah isi
 CHANNEL = os.environ.get("CHANNEL", "ofcbotxaiko") # Isi dengan username channel kalian tanpa tanda @ kalau gak punya gak usah isi
