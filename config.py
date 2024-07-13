f5085fa76008f16a2d58988694ed8bca87dbb1bb
IkoKoyi <magnumx019@gmail.com>
2024-07-14 00:32:40 +0700

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,17 @@
 import os
+from dotenv import load_dotenv
 
-TOKEN = os.environ.get("TOKEN", None) # Dapatkan di @botfather
-OWNER = os.environ.get("OWNER", "skoyi19") # Isi dengan username kalian tanpa tanda @
-GROUP = os.environ.get("GROUP", "Cari_pacar_jodoh_teman") # Isi dengan username group kalian tanpa tanda @ kalau gak punya gak usah isi
-CHANNEL = os.environ.get("CHANNEL", "ofcbotxaiko") # Isi dengan username channel kalian tanpa tanda @ kalau gak punya gak usah isi
-DB_URI = os.environ.get("DATABASE_URL", "")
+# Memuat variabel lingkungan dari file .env jika ada
+load_dotenv()
+
+# Mendapatkan variabel lingkungan
+TOKEN = os.environ.get("7422100791:AAHH8hkoriac0gLDPWH6bqnxW1EoqITSPKI")  # Dapatkan di @botfather
+if not TOKEN:
+    raise ValueError("Bot token is not defined. Please set the TOKEN environment variable.")
+
+OWNER = os.environ.get("OWNER", "skoyi19")  # Isi dengan username kalian tanpa tanda @
+GROUP = os.environ.get("GROUP", "Cari_pacar_jodoh_teman")  # Isi dengan username grup kalian tanpa tanda @ kalau gak punya gak usah isi
+CHANNEL = os.environ.get("CHANNEL", "ofcbotxaiko")  # Isi dengan username channel kalian tanpa tanda @ kalau gak punya gak usah isi
+DB_URI = os.environ.get("DATABASE_URL")  # Database URI
+if not DB_URI:
+    raise ValueError("Database URI is not defined. Please set the DATABASE_URL environment variable.")
